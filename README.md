fd8664132e4607d003504add59d9020dfc499230
kayacchang <egg734631@gmail.com>
2021-04-14 21:09:51 +0800

    ready to go

---
+++ README.md
@@ -0,0 +1,8 @@
+# HelloxPassingxDown - Gods Fortune Teller
+
+Hello 🌎
+Build a Gods Fortune Teller for help me find a girl friend!!!
+
+- [Mockup](https://www.figma.com/file/6p8txADploiZdCEj9liN6U/GOD%E2%80%99s-FORTUNE-TELLER?node-id=0%3A1)
+- [Youtube](https://youtu.be/YQNTRIY6dsw)
+- [Example](https://kayac-chang.github.io/helloxpassingxdown-gods-fortune-teller/)
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
+const { resolve } = require("path");
+
 export default {
-  base: "/helloxpassingxdown-gods-fortune-teller/",
+  base: "https://kayac-chang.github.io/helloxpassingxdown-gods-fortune-teller/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        choose: resolve(__dirname, "choose.html"),
+        divination: resolve(__dirname, "divination.html"),
+        result: resolve(__dirname, "result.html"),
+      },
+    },
+  },
 };
