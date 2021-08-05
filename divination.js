b16e4ef968bcdd4260ea0d39777ca5d73f9d7256
Yun Wen Chen <sssh10110@gmail.com>
2021-08-06 01:07:51 +0800

    finish basic function

--- divination.js
+++ divination.js
@@ -13,7 +13,8 @@ function pickFrom(list) {
   return list[(list.length * Math.random()) | 0];
 }
 
-export default function gameloop() {
+// export default 
+function gameloop() {
   let win = 0;
 
   return function random() {
@@ -25,3 +26,63 @@ export default function gameloop() {
     return { left, right, result, win };
   };
 }
+
+// From html
+const playOnce = gameloop();
+
+// divination blocks (n.) 筊杯
+function BuaBuey(el, type) {
+    const img = el.querySelector('img');
+
+    return {
+        run(result) {
+            // Eric TODO
+            el.classList.add('run');  // 下落動畫的class
+            img.src = `public/Divination_${type}_${result === 0 ? 'Close' : 'Open'}.png`
+            el.addEventListener('animationend', () => {
+              el.classList.remove('run'); // 移除下落動畫的class，避免動畫只顯現一次
+            })
+            return result;
+        }
+    }
+}
+
+function log({ left, right, result, win }) {
+    // Eric TODO
+    document.querySelector('[data-win-count]').dataset.winCount = win;
+    document.querySelector('#win-count').innerHTML = win;
+    // querySelector 會選到第一個符合的
+}
+
+function shake(el) {
+    const result = playOnce();
+
+    console.log(result);
+
+    log(result);
+
+    const left = BuaBuey(el.querySelector('div:first-child'), 'L');
+    // 選它之下的第一個div
+    const right = BuaBuey(el.querySelector('div:last-child'), 'R');
+    // 選它之下的最後一個div                // nth-child(2) 選第二個?
+
+    left.run(result.left);
+    right.run(result.right);
+
+    // Eric TODO
+
+    // 1. if not win
+    if(result.result === 0){
+      el.addEventListener('animationend', () => {
+        el.classList.remove('run'); // 移除下落動畫的class，避免動畫只顯現一次
+        window.location = 'choose.html';
+      })
+    }
+    // 2. if win
+    if(result.win === 3){
+      // console.log("WIN!");
+      window.location = 'result.html';
+    }
+}
+
+window.shake = shake;
